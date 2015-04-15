0b486808afa4b2787321007ca4c6049b10364407
Antonello Meloni <infovillasimius@gmail.com>
2015-04-15 23:27:59 +0200

    Esercizio 1 php

---
+++ test1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Esercizio 1</title>
+                <link href="nuovo.css" rel="stylesheet" type="text/css" media="screen" />
+	</head>
+	<body>
+		<?php		  	
+                    
+                    if(isset($_REQUEST["a"]) && isset($_REQUEST["b"])){
+                        $a = filter_var($_REQUEST["a"], FILTER_VALIDATE_INT);
+		  	$b = filter_var($_REQUEST["b"], FILTER_VALIDATE_INT);
+		  	$op= $_REQUEST["operatore"];
+                    }
+                    else {
+                        $a="";
+                        $b="";
+                        $op='+';
+                    }
+		  	
+                    if($op !='+' && $op !='-' && $op !='*' && $op !='/'){
+		  	$op='+';
+                    }
+		  	
+                    if ($a && $b){
+                        switch ($op) {
+                                case "+": $ris=$a+$b; break;
+                                case "-": $ris=$a-$b; break;
+                                case "*": $ris=$a*$b; break;
+                                case "/": $ris=$a/$b; break;
+                        }
+                    }
+		?>	
+		  
+	  <form	 method="post" action="test1.php">	
+	  	<p>
+		  	<label for="a">A</label>	
+		  	<input type="text" name="a" id="a" value="<?= $a ?>"/>
+		  	<select name="operatore">
+		  		<option value="+" <?= $op == "+" ?  'selected="true"' : "" ?> >Addizione</option>
+		  		<option value="-" <?= $op == "-" ?  'selected="true"' : "" ?> >Sottrazione</option>
+		  		<option value="*" <?= $op == "*" ?  'selected="true"' : "" ?> >Moltiplicazione</option>
+		  		<option value="/" <?= $op == "/" ?  'selected="true"' : "" ?> >Divisione</option>
+		  	</select>
+		  	<label for="b">B</label>	
+		 	<input type="text" name="b" id="b" value="<?= $b ?>"/>	
+		  	<input type="submit" value="Calcola"/>	
+	  	</p>
+	  </form>	
+	  
+	  <p>
+	  	<? 
+		  	if(isset($ris)){
+		  		echo "$a $op $b = $ris";
+		  	}
+		?> 
+	  </p>	
+	</body>
+</html>  	
+  	
+  
+  
+  
