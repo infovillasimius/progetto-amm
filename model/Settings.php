de4c4cbe2b402aa13f3095b82b13928b2f855776
Antonello Meloni <infovillasimius@gmail.com>
2015-06-09 09:20:54 +0200

    modifica per server pubblico

--- model/Settings.php
+++ model/Settings.php
@@ -4,12 +4,12 @@
  * Classe che contiene i parametri per la connessione al database
  */
 class Settings{
-//    public static $db_host='localhost';
-//    public static $db_user='meloniAntonello';
-//    public static $db_pass='bradipo6598';
-//    public static $db_name='amm15_meloniAntonello';
     public static $db_host='localhost';
-    public static $db_user='suap';
-    public static $db_pass='suap2015';
-    public static $db_name='suap';
+    public static $db_user='meloniAntonello';
+    public static $db_pass='bradipo6598';
+    public static $db_name='amm15_meloniAntonello';
+//    public static $db_host='localhost';
+//    public static $db_user='suap';
+//    public static $db_pass='suap2015';
+//    public static $db_name='suap';
 }
