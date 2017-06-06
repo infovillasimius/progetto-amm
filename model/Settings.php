614e7f8643449ec16ef7c7de5974301cb93bae63
Antonello Meloni <infovillasimius@users.noreply.github.com>
2017-06-06 14:14:20 +0200

    Update Settings.php

--- model/Settings.php
+++ model/Settings.php
@@ -8,8 +8,4 @@ class Settings{
 //    public static $db_user='meloniAntonello';
 //    public static $db_pass='bradipo6598';
 //    public static $db_name='amm15_meloniAntonello';
-    public static $db_host='localhost';
-    public static $db_user='suap';
-    public static $db_pass='suap2015';
-    public static $db_name='suap';
 }
